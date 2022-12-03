eb56ff7b8f69c3bd02ff294b27fc7f854bdd6ee7
John Hauge <jghauge@gmail.com>
2022-12-03 18:08:32 +0100

    Less blocky snow and open upm

--- Assets/Scripts/Runtime/SnowBlock.cs
+++ Assets/Scripts/Runtime/SnowBlock.cs
@@ -8,18 +8,21 @@ namespace SnowBlocks
         private readonly Vector3 _spawnPosition;
         private readonly Mesh _mesh;
         private readonly Material _material;
+        private readonly Camera _camera;
 
         public SnowBlock(Vector3 position, Mesh mesh, Material material)
         {
             _spawnPosition = position;
             _position = _spawnPosition;
+            _position.y = Random.Range(0f, _position.y);
             _mesh = mesh;
             _material = material;
+            _camera = Camera.main;
         }
 
         public void Update()
         {
-            var noise = Mathf.PerlinNoise(_position.x, _position.y);
+            var noise = Mathf.PerlinNoise(-1f, 1f);
             if (_position.y >= 0.5f)
             {
                 _position += Vector3.down * Time.deltaTime;
@@ -27,7 +30,8 @@ namespace SnowBlocks
             }
             else _position = _spawnPosition;
 
-            Graphics.DrawMesh(_mesh, _position, Quaternion.identity, _material, 1);
+            Graphics.DrawMesh(_mesh, _position, Quaternion.LookRotation(_camera.transform.forward), _material, 1,
+                null, 0, null, false, false, false);
         }
     }
 }
